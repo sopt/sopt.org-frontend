d9021593a9e21202b187e11c8b448557d1e2a134
nahee-park <jessica396@ewhain.net>
2022-06-03 01:57:14 +0900

    dev: 폴더구조, 스타일 초기세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Global, ThemeProvider } from '@emotion/react';
+import { global } from '@src/styles/global';
+import theme from '@src/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={global} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { Theme } from '@emotion/react';
+import { mainColor } from '@src/assets/replaceMe/mainColor';
+const theme: Theme = {
+  colors: {
+    mainColor: mainColor.mainColor,
+    mainBannerColor: mainColor.mainBannerColor,
+  },
+};
+export default theme;
