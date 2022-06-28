2b4b438f0c7d02b8d1e2b8d9f93c522d2ad1a0af
Danna <daeunlee0720@gmail.com>
2022-06-29 00:37:35 +0900

    feat: 타이틀 스타일 추가

--- src/components/Home/Top/Top.style.tsx
+++ src/components/Home/Top/Top.style.tsx
@@ -5,9 +5,11 @@ export const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
   background-image: url(${bgImg.src});
   background-repeat: no-repeat;
   background-size: cover;
+
   width: 100vw;
   height: 100vh;
 `;
@@ -17,22 +19,51 @@ export const TextWrap = styled.div`
   margin-left: 200px;
 `;
 
-export const SoptTitle = styled.h1``;
+export const SoptTitle = styled.h1`
+  line-height: 110%;
+  font-family: 'SUIT';
+  font-size: 100px;
+  font-weight: 900;
+  font-style: normal;
+`;
 
 export const SoptSubtitle = styled.h2`
   margin-top: 30px;
+  line-height: 62px;
+  letter-spacing: -0.03em;
+
+  font-family: 'SUIT';
+  font-size: 50px;
+  font-weight: 700;
+  font-style: normal;
 `;
 
 export const SoptDescription = styled.h3`
   margin-top: 107px;
+  line-height: 160%;
+
+  font-family: 'SUIT';
+  font-size: 30px;
+  font-weight: 500;
+  font-style: normal;
 `;
 
 export const ScrollWrap = styled.div`
+
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  margin-bottom: 40px;
+
   width: 100%;
 `;
 
-export const ScrollText = styled.p``;
+export const ScrollText = styled.p`
 
-export const ScrollArrow = styled.image``;
+  margin-bottom: 20px;
+  text-align: center;
+  font-family: 'SUIT';
+  font-size: 20px;
+  font-weight: 500;
+  font-style: normal;
+`;
