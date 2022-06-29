f43d66cae4ac359f57dfc51b3265b999e46e66d9
Danna <daeunlee0720@gmail.com>
2022-06-29 20:25:09 +0900

    fix: 최종합격 동그라미 스타일 변경

--- src/components/Home/Recruit/Recruit.style.tsx
+++ src/components/Home/Recruit/Recruit.style.tsx
@@ -24,17 +24,38 @@ export const StepWrap = styled.div`
 `;
 
 export const PlainCircleText = styled.p`
+
   margin-right: 30px;
 
   border: 4px solid ${({ theme }) => theme.colors.soptWhite};
   border-radius: 150px;
   padding: 72px 48px;
+  /* identical to box height */
+
+  text-align: center;
+  line-height: 35px;
+  letter-spacing: -0.04em;
+  font-family: 'SUIT', arial;
+  font-size: 28px;
+  font-weight: 500;
+  font-style: normal;
 `;
 
 export const GradientCircleText = styled.p`
+
   border: 4px solid ${({ theme }) => theme.colors.mainColor};
   border-radius: 150px;
-  padding: 72px 48px;
+  padding: 72px 40px;
+  /* identical to box height */
+
+  text-align: center;
+  line-height: 35px;
+  letter-spacing: -0.04em;
+  color: ${({ theme }) => theme.colors.mainColor};
+  font-family: 'SUIT', arial;
+  font-size: 28px;
+  font-weight: 500;
+  font-style: normal;
 `;
 
 export const ArrowIcon = styled.img`
