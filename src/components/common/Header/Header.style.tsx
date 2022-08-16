67c1ccce487cd4a9baf08571e65c24a85ea11911
Danna <daeunlee0720@gmail.com>
2022-08-17 05:05:04 +0900

    feat: 헤더 추가

--- src/components/common/Header/Header.style.tsx
+++ src/components/common/Header/Header.style.tsx
@@ -1,13 +1,24 @@
 import styled from '@emotion/styled';
+import menuBar from '@src/assets/icons/menuBar.svg';
+import logoIcon from '@src/assets/replaceMe/branding/logo.png';
 
 export const Header = styled.header`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 20%;
+
+  position: fixed;
+  justify-content: space-between;
+  margin: 30px 60px;
+  width: calc(100% - 120px);
 `;
 
-export const title = styled.h1`
-  color: ${({ theme }) => theme.colors.mainColor};
+export const Logo = styled.button`
+  background: blue;
+  width: 125px;
+  height: 41px;
+`;
+
+export const HamburgerBar = styled.button`
+  background: red;
+  width: 24px;
+  height: 24px;
 `;
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import * as S from './Header.style';
 
-export const ASSETS = {
-  TITLE: '메인 헤더입니다',
-};
-
 function Header() {
   return (
     <S.Header>
-      <S.title>{ASSETS.TITLE}</S.title>
+      <S.Logo />
+      <S.HamburgerBar />
     </S.Header>
   );
 }
