b90fd38d314901935510ead1b08a385d7b0a88f5
nahee-park <nahee.developer@gmail.com>
2022-08-20 04:36:02 +0900

    chore: 불필요한 주석 삭제

--- src/components/Rules/Rules.style.tsx
+++ src/components/Rules/Rules.style.tsx
@@ -5,8 +5,6 @@ export const Root = styled.main`
   margin-bottom: 130px;
   width: 100%;
   max-width: 1192px;
-  /* height: 100vh;
-  overflow-y: scroll; */
   @media screen and (max-width: 1280px) {
     width: 92%;
   }
