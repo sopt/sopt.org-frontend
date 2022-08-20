4a74c80c9c145a9af4b99d2d3366c6d9d53cb49b
nahee-park <nahee.developer@gmail.com>
2022-08-20 16:49:24 +0900

    feat: image, async babel setting

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['sopt-makers.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 module.exports = nextConfig;
